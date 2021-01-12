16abe5a35af61a29ba62259be0b7e7f7e83ce464
Ali Najdi <ali.najdi.1989@gmail.com>
2021-01-12 16:46:10 +0330

    added statistics component

--- src/components/Statistics/index.js
+++ src/components/Statistics/index.js
@@ -4,10 +4,44 @@ const Index = () => {
     return (
         <div className={styles.statsWrapper}>
             <h1 className={styles.title}>الگوریتم، ضامن سرمایه گذاری شما</h1>
-            <div>
+            <div className={styles.statItems}>
                 <div className={styles.item}>
+                    <img className={styles.icon} src="/images/investment-icon.svg" alt=""/>
+                    <div className={styles.text}>
+                        <h4 className={styles.statTitle}>
+                            23,679 میلیارد
+                            <span className={styles.detail}>ریال</span>
+                        </h4>
+                        <p className={styles.description}> خالص ارزش دارایی های تحت مدیریت</p>
+                    </div>
 
                 </div>
+
+                <div className={styles.item}>
+                    <img className={styles.icon} src="/images/investors-icon.svg" alt=""/>
+                    <div className={styles.text}>
+                        <h4 className={styles.statTitle}>
+                           4125
+                        </h4>
+                        <p className={styles.description}>تعداد سرمایه گذاران</p>
+                    </div>
+
+                </div>
+
+                <div className={styles.item}>
+                    <img className={styles.icon} src="/images/advisors-icon.svg" alt=""/>
+                    <div className={styles.text}>
+                        <h4 className={styles.statTitle}>
+                            12
+
+                        </h4>
+                        <p className={styles.description}>تعداد مشاوران</p>
+                    </div>
+
+                </div>
+
+
+
             </div>
         </div>
     );
